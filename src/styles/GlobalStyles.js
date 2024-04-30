d148eceff6f85a376d485a6320054984b8dfecf7
Gilvan Oliveira <gilvanoliveira06@gmail.com>
2024-04-30 09:14:19 -0300

    :sparkles: Vai Na Web

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,12 +2,11 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     * {
-        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap');
         margin: 0;
         padding: 0;
         box-sizing: border-box;
         text-decoration: none;
-        font-family: 'Inter', sans-serif;
+        font-family: 'Roboto', sans-serif;
     } 
     h2 {
         font-size: clamp(1.5rem, 2.5vw, 3rem);
